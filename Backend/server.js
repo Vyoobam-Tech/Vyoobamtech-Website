755c1edea734a9bbc3a41fd754140d74de544ffe
Keerthivashini <keerthivashini5@gmail.com>
2025-12-30 01:33:09 -0800

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const multer = require("multer");
